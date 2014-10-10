1db02e3102a7f391731485de13e7f18aaa3fd8fd
alfred sang <i5ting@126.com>
2014-10-10 19:43:06 +0800

    fix jsonkit repo issue

--- bin/submodule_prepare.sh
+++ bin/submodule_prepare.sh
@@ -4,13 +4,14 @@
 
 export BASE_PATH=${PWD}
 
+mkdir -p $BASE_PATH/src/vendor
 echo $BASE_PATH
 
 cd $BASE_PATH/src/vendor
 
 while read line
 do
-	git submodule add git@github.com:"$line".git
+	git submodule add  --force git@github.com:"$line".git 
 done < $BASE_PATH/conf/submodule_file
 
 # git@github.com: .git
